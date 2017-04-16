b6335fd04f5b32cafafc8efd3b032560d7cf32b4
ink <15519586771@163.com>
2017-04-17 07:22:20 +0800

    上传了一个fitness

---
+++ contact/js/component.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+var component=function(name,cfg){
+    var cfg=cfg;
+    var cls=('component_name'+name+' component_'+cfg.type)
+}
---
+++ work/work.js
@@ -0,0 +1,42 @@
+/**
+ * Created by lenovo on 2017/2/22.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function myScroll() {
+    return {
+        top: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0,
+        left: window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0
+    };
+}
